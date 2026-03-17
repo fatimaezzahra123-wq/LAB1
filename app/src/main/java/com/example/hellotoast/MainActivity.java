5ef87fd14c89545ce97e59540140a80ab19d3716
fatimaezzahra <f.ennassiri6175@uca.ac.ma>
2026-03-17 08:37:02 +0100

    lab1

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+
+    private TextView textCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textCount = findViewById(R.id.button_count);
+        Button buttonToast = findViewById(R.id.button_toast);
+        Button buttonCount = findViewById(R.id.button_count);
+
+        buttonToast.setOnClickListener(v -> {
+            Toast.makeText(this, "Hello TOAST!", Toast.LENGTH_SHORT).show();
+        });
+
+        buttonCount.setOnClickListener(v -> {
+            count++;
+            textCount.setText(String.valueOf(count));
+        });
+    }
+}
